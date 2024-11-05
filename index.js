bdd2a1ece6d2829138a49e32ad7a2c14b3f3fa52
Sandi Junker <sandi.junker@outlook.com>
2024-11-05 16:37:32 -0600

    Begin css, begin JS and JSON files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var serveStatic = require('serve-static');
 var app = express();
 
 app.use(serveStatic('public', { index: '/index.html' }));
+app.use('/css', express.static('public' + '/css'));
 
 app.listen(3000);
 
---
+++ public/scripts/loadProjects.mjs
@@ -0,0 +1,3 @@
+//export render projects (takes in an array of projects and renders to html)
+
+//export read projects from json, takes in a file of projects (current and completed)
