6b3cf40c11b6a0003905f1c01a032d2ac2679365
Sandi Junker <sandi.junker@outlook.com>
2024-11-05 06:09:31 -0600

    Start up project so that index.html launches after running index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+var app = express();
+
+app.use(serveStatic('public', { index: '/index.html' }));
+
+app.listen(3000);
+
+console.log('Server running at http://127.0.0.1:3000/');
